9bbe79be61ddf0c8d6b07cbb07d3b18820e891ca
Aleksandra LUKIC <aleksandra.lukic@inetum.altia.es>
2021-10-24 21:00:00 +0200

    Added the first sanity test

---
+++ src/main/java/GhostTest.java
@@ -0,0 +1,56 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class GhostTest {
+
+    public String environment_url = "http://localhost/";
+
+    public static String SUBSCRIBE_BUTTON = "//a[@class='gh-head-button']";
+    public static String PAGE_TITLE = "//h1[@class='site-title']";
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    void test() {
+        WebElement pageTitle = driver.findElement(By.xpath(PAGE_TITLE));
+        driver.get(environment_url);
+        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.visibilityOf(pageTitle));
+        driver.findElement(By.xpath(PAGE_TITLE)).getText();
+        assertTrue(pageTitle.isDisplayed());
+    }
+}
+
