b50651af8a08560b86e8f410c4defa8e3cc5ed97
Aleksandra LUKIC <aleksandra.lukic@inetum.altia.es>
2021-10-24 19:22:38 +0200

    Moving forward with the project

---
+++ README.md
@@ -0,0 +1,6 @@
+##Ghost application deployment tests
+
+###Scenarios
+1. Deploy an application with the default username and password and make sure that the application is deployed properly and user can log in to the app
+2. Deploy an application with the ghost blog title matching the username. Then make sure that the deployment went well and the blog has changed the title.
+
---
+++ src/test/scenario1/setTheStage.sh
@@ -0,0 +1,35 @@
+### FIRST TEST SCENARIO
+### STEP 1 - CREATE NETWORK
+docker network create ghost-network
+
+### STEP 2 - CREATE PERSISTENT VOLUME MARIADB
+docker volume create --name mariadb_data
+docker run -d --name mariadb \
+  --env ALLOW_EMPTY_PASSWORD=yes \
+  --env MARIADB_USER=bn_ghost \
+  --env MARIADB_PASSWORD=bitnami \
+  --env MARIADB_DATABASE=bitnami_ghost \
+  --network ghost-network \
+  --volume mariadb_data:/bitnami/mariadb \
+  bitnami/mariadb:latest
+
+### STEP 3 - CREATE VOLUME FOR GHOST PERSISTENCE
+
+docker volume create --name ghost_data
+
+docker run -d --name ghost \
+  -p 8080:8080 -p 8443:8443 \
+  --env ALLOW_EMPTY_PASSWORD=yes \
+  --env GHOST_DATABASE_USER=bn_ghost \
+  --env GHOST_DATABASE_PASSWORD=bitnami \
+  --env GHOST_DATABASE_NAME=bitnami_ghost \
+  --env GHOST_USERNAME=aleksandra \
+  --env GHOST_PASSWORD=goodToBeAlive123 \
+  --env GHOST_EMAIL=aleksandra@yopmail.com \
+  --network ghost-network \
+  --volume ghost_data:/bitnami/ghost \
+  bitnami/ghost:latest
+
+### STEP 4 - START THE APPLICATION
+
+docker-compose up
