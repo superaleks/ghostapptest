0d8cbf1744d66a37c069db3b21e2ad2d54beeffc
Aleksandra LUKIC <aleksandra.lukic@inetum.altia.es>
2021-10-25 17:50:47 +0200

    Added the expanded version of the helm chart

--- src/test/helm/scenario1/setTheStage.sh
+++ src/test/helm/scenario1/setTheStage.sh
@@ -1,4 +1,10 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm install my-release \
   --set ghostBlogTitle=Aleksandra \
+  --set ghostPath=localhost  \
+  --set service.type=LoadBalancer  \
+  --set ghostHost=localhost  \
+  --set ghostPassword=test \
+  --set mariadb.auth.rootPassword=test \
+  --set mariadb.auth.password=test
     bitnami/ghost
